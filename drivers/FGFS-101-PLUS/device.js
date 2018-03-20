655447dea447ba016a7b8e180d519d4688034cf8
Matthew Waanders <matthew@athom.nl>
2018-03-20 16:03:58 +0100

    Processed Robin's feedback

--- drivers/FGFS-101-PLUS/device.js
+++ drivers/FGFS-101-PLUS/device.js
@@ -7,38 +7,13 @@ class FibaroFloodSensorPlus extends ZwaveDevice {
 	onMeshInit() {
         this.registerCapability('alarm_water', 'NOTIFICATION');
         this.registerCapability('alarm_tamper', 'NOTIFICATION');
-        this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
-        	getOpts: {
-        		getOnOnline: true,
-			}
-		});
+        this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
         this.registerCapability('measure_battery', 'BATTERY');
 
-        this.setCapabilityValue('alarm_water', false);
-        this.setCapabilityValue('alarm_tamper', false);
-
-        /*
-		=====================================================
-		General setting registration
-		=====================================================
-		*/
-        this.registerSetting('flood_sensor');
-        this.registerSetting('flood_signal');
-        this.registerSetting('alarm_duration');
-        this.registerSetting('alarm_cancellation_delay');
-        this.registerSetting('tamper_alarm');
-
-        /*
-		=====================================================
-		Temperature setting registration
-		=====================================================
-		*/
-        this.registerSetting('temperature_measure_interval');
-        this.registerSetting('temperature_measure_hysteresis');
-        this.registerSetting('temperature_measure_offset');
-        this.registerSetting('low_temperature_threshold');
-        this.registerSetting('high_temperature_threshold');
-
+        this.registerSetting('temperature_measure_hysteresis', value => value * 10);
+        this.registerSetting('temperature_measure_offset', value => value * 100);
+        this.registerSetting('low_temperature_threshold', value => value * 100);
+        this.registerSetting('high_temperature_threshold', value => value * 100);
     }
 	
 }
